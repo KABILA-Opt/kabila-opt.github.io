2c309cbafa47f51bcf0c43bd8cfc3bee13160cf2
changhz <hz.chang@pm.me>
2020-07-17 14:56:35 -0400

    menu

---
+++ script/Menu.js
@@ -0,0 +1,31 @@
+(function ($) {
+    var root = "/data/menu";
+    fetch(root + "/side-photo")
+        .then(function (r) { return r.text(); })
+        .then(function (r) {
+        $("#side-photo").attr("src", r.replace(/\s/g, ""));
+    });
+    var itemRoot = root + "/item-";
+    [1, 2, 3].forEach(function (n) {
+        ["photo", "text.html", "title"].forEach(function (k) {
+            fetch(itemRoot + n)
+                .then(function (r) { return r.text(); })
+                .then(function (r) {
+                var data = r.trim();
+                switch (k) {
+                    case "photo":
+                        var src = data.replace(/\s/g, "");
+                        $("#item-" + n + " .photo-cell").css("background-image", "url(" + src + ")");
+                        $("#item-" + n + " .photo").attr("src", src);
+                        break;
+                    case "title":
+                        $("#item-" + n + " .title").html(data);
+                        break;
+                    case "text":
+                        $("#item-" + n + " .title").html(data);
+                        break;
+                }
+            });
+        });
+    });
+})(jQuery);
---
+++ src/app/Menu.ts
@@ -0,0 +1,37 @@
+(($: JQueryStatic) => {
+  let root = "/data/menu";
+  fetch(root + "/side-photo")
+    .then((r) => r.text())
+    .then((r) => {
+      $("#side-photo").attr("src", r.replace(/\s/g, ""));
+    });
+
+  let itemRoot = root + "/item-";
+  [1, 2, 3].forEach((n) => {
+    ["photo", "text.html", "title"].forEach((k) => {
+      fetch(itemRoot + n)
+        .then((r) => r.text())
+        .then((r) => {
+          let data = r.trim();
+          switch (k) {
+            case "photo":
+              let src = data.replace(/\s/g, "");
+              $(`#item-${n} .photo-cell`).css(
+                "background-image",
+                `url(${src})`,
+              );
+              $(`#item-${n} .photo`).attr("src", src);
+              break;
+
+            case "title":
+              $(`#item-${n} .title`).html(data);
+              break;
+
+            case "text":
+              $(`#item-${n} .title`).html(data);
+              break;
+          }
+        });
+    });
+  });
+})(jQuery);
