98e8272af79423e91f5ec66adc77424856b92240
changhz <hz.chang@pm.me>
2020-07-20 20:44:10 -0400

    fixes

--- script/About.js
+++ script/About.js
@@ -1,26 +1,22 @@
 function About($) {
-    var title = $("#chef-title");
-    var text = $("#chef-text");
     fetch("/data/home/chef-text.html")
         .then(function (r) { return r.text(); })
         .then(function (r) {
-        text.html(r.trim());
+        $("#chef-text").html(r.trim());
     });
     fetch("/data/home/chef-title.html")
         .then(function (r) { return r.text(); })
         .then(function (r) {
-        title.html(r.trim());
+        $("#chef-title").html(r.trim());
     });
-    title = $("#about-title");
-    text = $("#about-text");
     fetch("/data/home/about-text.html")
         .then(function (r) { return r.text(); })
         .then(function (r) {
-        text.html(r.trim());
+        $("#about-text").html(r.trim());
     });
     fetch("/data/home/about-title.html")
         .then(function (r) { return r.text(); })
         .then(function (r) {
-        title.html(r.trim());
+        $("#about-title").html(r.trim());
     });
 }
--- src/app/About.ts
+++ src/app/About.ts
@@ -1,31 +1,25 @@
 function About($: JQueryStatic) {
-  let title = $("#chef-title");
-  let text = $("#chef-text");
-
   fetch("/data/home/chef-text.html")
     .then((r) => r.text())
     .then((r) => {
-      text.html(r.trim());
+      $("#chef-text").html(r.trim());
     });
 
   fetch("/data/home/chef-title.html")
     .then((r) => r.text())
     .then((r) => {
-      title.html(r.trim());
+      $("#chef-title").html(r.trim());
     });
-
-  title = $("#about-title");
-  text = $("#about-text");
 
   fetch("/data/home/about-text.html")
     .then((r) => r.text())
     .then((r) => {
-      text.html(r.trim());
+      $("#about-text").html(r.trim());
     });
 
   fetch("/data/home/about-title.html")
     .then((r) => r.text())
     .then((r) => {
-      title.html(r.trim());
+      $("#about-title").html(r.trim());
     });
 }
