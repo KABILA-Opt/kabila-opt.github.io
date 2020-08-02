0391a4e0b9597f5d8cdacf793d818892f3609a2f
changhz <hz.chang@pm.me>
2020-08-02 11:19:13 -0400

    ajouter un plat

--- task/menu/rename.ts
+++ task/menu/rename.ts
@@ -1,9 +1,10 @@
 import { renameSync } from "fs";
+import { join } from "path";
 
 (() => {
   ["en", "fr"].forEach((loc) => {
-    let root = `../data/${loc}/menu/item-`;
-    for (let i = 1; i < 11; i++) {
+    let root = join(__dirname, `../data/${loc}/menu/item-`);
+    for (let i = 11; i >= 1; i--) {
       renameSync(root + i, root + (i + 1));
     }
   });
