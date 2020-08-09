6956a3b90d15b84480c1518642f3cd3fc3fdbe73
changhz <hz.chang@pm.me>
2020-08-09 00:32:47 -0400

    mise a jour

--- task/menu/rename.ts
+++ task/menu/rename.ts
@@ -4,7 +4,7 @@ import { join } from "path";
 (() => {
   ["en", "fr"].forEach((loc) => {
     let root = join(__dirname, `../data/${loc}/menu/item-`);
-    for (let i = 12; i >= 7; i--) {
+    for (let i = 15; i >= 7; i--) {
       renameSync(root + i, root + (i + 1));
     }
   });
